278feb9021adfd000b66fc9fba9efb321f3d91f0
stennn <stennn@msn.com>
2024-03-12 18:17:12 +0200

    newspaper app

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("pages.urls")),
+    path("articles/", include("articles.urls")),
 ]
